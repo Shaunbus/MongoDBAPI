0f39c5ef18a2c99048ec16f5c676b191b573e339
Shaunbus <sjbucyk@live.ca>
2020-02-07 03:38:18 +0000

    working array of JSON GET

--- controllers/msg-api.js
+++ controllers/msg-api.js
@@ -1,6 +1,19 @@
+
+const messages = [
+   { name : "Bill", msg : "Hi All!"},
+   { name : "Ann", msg : "ICS 221 is fun!"},
+    { name : "Johnny", msg : "I'm stranded!"},
+    { name : "Barb", msg : "Hi"},
+    { name : "Frank", msg : "Who's tired?"},
+    { name : "Sarah", msg : "I heart React"}];
+
 // GET request handler
 const getAllMessages = (req,res) => {
-    res.status(200).send('Successful API GET Request');
+    if (typeof messages == 'object' && Array.isArray(messages)){
+    res.status(200).json(messages);
+    }else{
+        res.status(400).send('Bad Request');
+    }
 };
 
 // POST Request Handler
--- routes/api_router.js
+++ routes/api_router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 const msgAPIController = require('../controllers/msg-api');
 
 router.route('/messages')
