c924989c3b3368c9696eeed7cb94a76b7615892b
Shaunbus <sjbucyk@live.ca>
2020-02-11 23:05:33 +0000

    get workign with db

--- controllers/msg-api.js
+++ controllers/msg-api.js
@@ -1,20 +1,32 @@
 const messageSchema = require('../models/message_schema.js');
+const mongoose = require('mongoose');
+const messageModel = mongoose.model('message');
 
-const messages = [
-    { name : "Bill", msg : "Hi All!"},
-    { name : "Ann", msg : "ICS 221 is fun!"},
-    { name : "Johnny", msg : "I'm stranded!"},
-    { name : "Barb", msg : "Hi"},
-    { name : "Frank", msg : "Who's tired?"},
-    { name : "Sarah", msg : "I heart React"}];
+// const messages = [
+//     { name : "Bill", msg : "Hi All!"},
+//     { name : "Ann", msg : "ICS 221 is fun!"},
+//     { name : "Johnny", msg : "I'm stranded!"},
+//     { name : "Barb", msg : "Hi"},
+//     { name : "Frank", msg : "Who's tired?"},
+//     { name : "Sarah", msg : "I heart React"}];
 
 // GET request handler
 const getAllMessages = (req,res) => {
-    if (typeof messages == 'object' && Array.isArray(messages)){
-    res.status(200).json(messages);
-    }else{
-        res.status(400).send('Bad Request');
-    }
+    // if (typeof messages == 'object' && Array.isArray(messages)){
+    // res.status(200).json(messages);
+    // }else{
+    //     res.status(400).send('Bad Request');
+    // }
+    messageModel
+    .find()
+    .sort( {'_id': -1})
+    .exec( (error, messages) => {
+        if (error) {
+            res.status(400).send('Bad Request');
+        }else {
+            res.status(200).json(messages);
+        }
+    });
 };
 
 // POST Request Handler
